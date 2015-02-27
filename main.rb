cb9165d3a07f0ea33580a6a0d0cc15183bc6644a
Tomek Rojek <tomek@rojek.cc>
2015-02-27 08:54:23 +0100

    connect with Allegro, search item by name

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'savon'
+require 'digest'
+
+client = Savon.client do
+  wsdl "https://webapi.allegro.pl/service.php?wsdl"
+end
+
+cnf = YAML.load_file('config.yml')
+
+haslo  = Digest::SHA256.base64digest cnf['password']
+
+local_versioon_response = client.call(:do_query_sys_status, message: {'sysvar' => 1, 
+                                                                      'countryId' => 1, 
+                                                                      'webapiKey' => cnf['webapikey']})
+
+login_response = client.call(:do_login_enc, message: { 'userLogin' => cnf['user'], 
+                                                       'userHashPassword' => haslo,
+                                                       'countryCode' => 1, 'webapiKey' => cnf['webapikey'],
+                                                       'localVersion' => local_versioon_response.hash[:envelope][:body][:do_query_sys_status_response][:ver_key] })
+
+search_response = client.call(:do_search, message: { 'sessionHandle' => login_response.hash[:envelope][:body][:do_login_enc_response][:session_handle_part],
+                                                     'searchQuery' => { 'searchString' => 'ipad'} })
+
+puts search_response.hash
